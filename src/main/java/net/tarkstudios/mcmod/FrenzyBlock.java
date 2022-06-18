792f83798db7ca5ba2a901fcddb7311e02b602fa
Tarku <v.susini@laposte.net>
2022-06-18 22:12:38 +0200

    Initial commit

---
+++ src/main/java/net/tarkstudios/mcmod/FrenzyBlock.java
@@ -0,0 +1,9 @@
+package net.tarkstudios.mcmod;
+
+import net.minecraft.block.Block;
+
+public class FrenzyBlock extends Block {
+    public FrenzyBlock(Settings settings) {
+        super(settings);
+    }
+}
---
+++ src/main/java/net/tarkstudios/mcmod/MCMod.java
@@ -0,0 +1,32 @@
+package net.tarkstudios.mcmod;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MCMod implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger("mcmod");
+	public static final FrenzyBlock FRENZY_BLOCK = new FrenzyBlock(FabricBlockSettings.of(Material.STONE).strength(1.5f));
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		Registry.register(Registry.BLOCK, new Identifier("mcmod", "frenzy_block"), FRENZY_BLOCK);
+		Registry.register(Registry.ITEM, new Identifier("mcmod", "frenzy_block"), new BlockItem(FRENZY_BLOCK, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
