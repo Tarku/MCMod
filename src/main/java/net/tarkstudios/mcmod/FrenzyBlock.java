c4b6b0c37eadd8397e6a3602d24dcfb7289fe7fc
Tarku <v.susini@laposte.net>
2022-06-18 22:20:39 +0200

    tesd

--- src/main/java/net/tarkstudios/mcmod/FrenzyBlock.java
+++ src/main/java/net/tarkstudios/mcmod/FrenzyBlock.java
@@ -1,9 +1,27 @@
 package net.tarkstudios.mcmod;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class FrenzyBlock extends Block {
     public FrenzyBlock(Settings settings) {
         super(settings);
+    }
+
+    @Override
+    public ActionResult onUse(BlockState blockState, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit)
+    {
+        if(!world.isClient)
+        {
+            player.sendMessage(Text.of("Hello World!"), false);
+        }
+        return ActionResult.SUCCESS;
     }
 }
